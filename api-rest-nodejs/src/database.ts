a60fe60b878290c68fe1eed6eb1e59269d8eebd6
Italo Guasti <italo.gguas@gmail.com>
2026-03-03 11:14:24 -0300

    chore: production config

--- api-rest-nodejs/src/database.ts
+++ api-rest-nodejs/src/database.ts
@@ -1,16 +1,19 @@
-import knex from 'knex'
-import { env } from './env/index.js'
+import knex from "knex";
+import { env } from "./env/index.js";
 
 export const config = {
-  client: 'sqlite3',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: "sqlite3",
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
-    extension: 'ts',
-    directory: './db/migrations',
+    extension: "ts",
+    directory: "./db/migrations",
   },
-}
+};
 
-export const dbKnex = knex(config)
+export const dbKnex = knex(config);
